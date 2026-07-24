91c976039b41e8c69a7f0121f89d88aff258ee37
Developer 0 <developer0@uncom.tech>
2026-07-24 08:41:59 +0300

    Remove unused show_completion_popup function from dialogs.py

--- usr/lib/uncom/typy/dialogs.py
+++ usr/lib/uncom/typy/dialogs.py
@@ -3,25 +3,6 @@ from gi.repository import Adw, GLib, Gtk
 from constants import _
 
 
-def show_completion_popup(window, callback_restart):
-    dialog = Gtk.AlertDialog()
-    dialog.set_message(_("Finished!"))
-    dialog.set_detail(_("You completed the typing test."))
-    dialog.set_buttons([_("Restart"), _("Close")])
-    dialog.set_default_button(0)
-    dialog.set_cancel_button(1)
-
-    def on_response(source, result, *_data):
-        try:
-            button_index = dialog.choose_finish(result)
-        except GLib.Error:
-            return
-        if button_index == 0:
-            callback_restart()
-
-    dialog.choose(window, None, on_response)
-
-
 def show_settings(window):
     dialog = Adw.PreferencesDialog()
     page = Adw.PreferencesPage()
