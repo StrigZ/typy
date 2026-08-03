63840a22d5cb45874d96d4bdb865c43c4046ac58
Developer 0 <developer0@uncom.tech>
2026-08-03 18:51:53 +0300

    Refactor DailyGoal class to add streaks

--- usr/lib/uncom/typy/daily_goal.py
+++ usr/lib/uncom/typy/daily_goal.py
@@ -1,19 +1,26 @@
 import json
 import os
-from datetime import date
+from datetime import date, timedelta
 
 from constants import DAILY_GOAL_FILE
 from utility import ensure_folder_exists
 
 
 class DailyGoal:
-    # TODO: get goal from config file
-    def __init__(self, goal: int = 1):
-        self.goal_in_minutes = goal
-        self.elapsed_in_minutes: float = 0.0
-        self.date: str = date.today().isoformat()
-
+    def __init__(self):
         self._load_data()
+
+        today = date.today().isoformat()
+        today_data = self.data.get(today, {})
+
+        self.date = today
+
+        self.goal_in_minutes = today_data.get("goal_in_minutes", 15)
+        self.elapsed_in_minutes = today_data.get("elapsed_in_minutes", 0.0)
+        self.streak = today_data.get("streak", 0)
+        self.was_goal_reached = today_data.get("was_goal_reached", False)
+
+        self._check_new_day()
 
     @property
     def is_goal_reached(self) -> bool:
@@ -33,7 +40,20 @@ class DailyGoal:
 
     def increment(self, elapsed_in_seconds: float):
         self._check_new_day()
+
         self.elapsed_in_minutes += elapsed_in_seconds / 60
+
+        if not self.was_goal_reached and self.is_goal_reached:
+            self.was_goal_reached = True
+
+            yesterday_str = (date.today() - timedelta(days=1)).isoformat()
+            yesterday_data = self.data.get(yesterday_str, {})
+
+            if yesterday_data.get("was_goal_reached"):
+                self.streak = yesterday_data.get("streak", 0) + 1
+            else:
+                self.streak = 1
+
         self._save_data()
 
     def set_goal(self, goal: int):
@@ -42,30 +62,40 @@ class DailyGoal:
 
     def _check_new_day(self):
         today = date.today().isoformat()
-        if today != self.date:
-            self.date = today
-            self.elapsed_in_minutes = 0.0
+        yesterday_str = (date.today() - timedelta(days=1)).isoformat()
+
+        # if the same day, do nothing
+        if today == self.date:
+            return
+
+        # if new day, reset props
+        self.date = today
+        self.elapsed_in_minutes = 0
+        self.was_goal_reached = False
+
+        yesterday_data = self.data.get(yesterday_str, {})
+        if yesterday_data.get("was_goal_reached"):
+            self.streak = yesterday_data.get("streak", 0)
+        else:
+            self.streak = 0
+
+        self._save_data()
 
     def _save_data(self):
         ensure_folder_exists(os.path.dirname(DAILY_GOAL_FILE))
+        self.data[self.date] = {
+            "goal_in_minutes": self.goal_in_minutes,
+            "elapsed_in_minutes": self.elapsed_in_minutes,
+            "was_goal_reached": self.was_goal_reached,
+            "streak": self.streak,
+        }
+
         with open(DAILY_GOAL_FILE, "w", encoding="utf-8") as f:
-            json.dump(
-                {
-                    "goal_in_minutes": self.goal_in_minutes,
-                    "elapsed_in_minutes": self.elapsed_in_minutes,
-                    "goal_reached": self.is_goal_reached,
-                    "date": self.date,
-                },
-                f,
-            )
+            json.dump(self.data, f)
 
     def _load_data(self):
         try:
             with open(DAILY_GOAL_FILE, "r", encoding="utf-8") as f:
-                data = json.load(f)
-            self.goal_in_minutes = data.get("goal_in_minutes", self.goal_in_minutes)
-            self.elapsed_in_minutes = data.get("elapsed_in_minutes", 0.0)
-            self.date = data.get("date", self.date)
-            self._check_new_day()
+                self.data = json.load(f)
         except (FileNotFoundError, json.JSONDecodeError):
-            pass
+            self.data = {}
